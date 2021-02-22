dc13982e24c2e93f832e54c826186ac2c05491d9
David Moreno <davidan7845@gmail.com>
2021-02-21 21:31:56 -0500

    Primer Commit jeje

---
+++ src/craps/PrincipalCraps.java
@@ -0,0 +1,25 @@
+/*
+ * Programación Interactiva
+ * Autor: David Andres Moreno - 1942750-2711
+ * Caso 1: Juego Craps
+ */
+package craps;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class PrincipalCraps. Clase que contiene el método main e inicia el programa
+ */
+public class PrincipalCraps {
+
+	/**
+	 * The main method. Método principal en java 
+	 *
+	 * @param args the arguments
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		VistaConsola consola = new VistaConsola();
+		consola.iniciarJuego();
+	}
+
+}
